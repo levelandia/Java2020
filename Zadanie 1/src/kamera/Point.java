d2b3ba140f72eca3f6339784e46b4e3a6850c3f8
levelandia <47124937+levelandia@users.noreply.github.com>
2020-08-03 18:02:28 +0200

    Add files via upload

---
+++ Zadanie 1/src/kamera/Point.java
@@ -0,0 +1,9 @@
+
+package kamera;
+
+public class Point {
+    
+    int x=0;
+    int y=0;
+    
+}
---
+++ Zadanie 1/src/kamera/Rectangle.java
@@ -0,0 +1,7 @@
+
+package kamera;
+
+public class Rectangle {
+     int width=600;
+     int height=500;
+}
