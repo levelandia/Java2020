01583a4f7e0405c496325a567d5ed9e27732a741
levelandia <47124937+levelandia@users.noreply.github.com>
2020-08-03 18:16:01 +0200

    Add files via upload

---
+++ Zadanie 4/src/Szkola.java
@@ -0,0 +1,10 @@
+
+public class Szkola extends Uczen
+{
+	String NazwaSzkoly;
+	public void UstawSzkole (String szkola)
+	{
+		this.NazwaSzkoly = szkola;
+	}
+
+}
---
+++ Zadanie 4/src/Uczen.java
@@ -0,0 +1,23 @@
+
+public class Uczen 
+{
+String Imie;
+String Nazwisko;
+String Pesel;
+
+	public void UstawImie (String imie)
+	{
+		this.Imie = imie;
+	}
+	
+	public void UstawNazwisko (String nazwisko)
+	{
+		this.Nazwisko = nazwisko;
+	}
+	
+	public void UstawPesel (String pesel)
+	{
+		this.Pesel = pesel;
+	}
+
+}
